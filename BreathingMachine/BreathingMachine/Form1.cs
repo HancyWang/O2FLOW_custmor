3d2ccc5e71fe9fe47d04cc8488d035441fcf7a41
HancyWang <wanghouxun@126.com>
2018-11-06 21:10:07 +0800

    更新，容错

--- BreathingMachine/BreathingMachine/Form1.cs
+++ BreathingMachine/BreathingMachine/Form1.cs
@@ -610,7 +610,21 @@ namespace BreathingMachine
             else
             {
                 //如果工作信息文件没有，则使用报警文件的
-                FileStream fs = new FileStream(FileMngr.m_dirPath + @"\" + FileMngr.m_alarmFileName, FileMode.Open);
+
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(FileMngr.m_dirPath + @"\" + FileMngr.m_alarmFileName, FileMode.Open);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                finally
+                {
+
+                }
                 BinaryReader br = new BinaryReader(fs, Encoding.ASCII);
 
                 //读信息头
@@ -4135,7 +4149,20 @@ namespace BreathingMachine
             {
                 //工作信息文件缺失时，使用报警文件的
                 #region
-                FileStream fs = new FileStream(m_dirPath + @"\" + m_alarmFileName, FileMode.Open);
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(m_dirPath + @"\" + m_alarmFileName, FileMode.Open);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                finally
+                {
+
+                }
                 BinaryReader br = new BinaryReader(fs, Encoding.ASCII);
 
                 //读信息头
@@ -4205,13 +4232,21 @@ namespace BreathingMachine
             }
             else
             {
-                alarmFilePath = null;
+                //alarmFilePath = null;
+                m_alarmFileName = null;
             }
             
             var workDataFilePathes = Directory.GetFiles(m_dirPath, "DATA*.vmf");
             
             foreach (var file in workDataFilePathes)
             {
+                var name = file;
+                name = name.Substring(name.LastIndexOf(@"\") + 1);
+                if (name.Length != 16)
+                    continue;
+                System.IO.FileInfo fileInfo = new FileInfo(file);
+                if (fileInfo.Length <= 1088)
+                    continue;
                 m_workFileNameList.Add(file.Substring(file.LastIndexOf(@"\") + 1)); //将工作文件名添加到链表中
             }
         }
@@ -4378,15 +4413,17 @@ namespace BreathingMachine
                     byte[] buffer_msg = new byte[len_msg];
 
 
-                    if (br.Read(buffer_msg, 0, len_msg) == 0)
+                    int read_len = 0;
+                    for (; ; )
                     {
-                        //do nothing
-                        fs.Close();
-                        br.Close();
-                    }
-                    else
-                    {
-                        while (br.Read(buffer_msg, 0, len_msg) > 0)
+                        read_len = br.Read(buffer_msg, 0, len_msg);
+                        if (read_len == 0)
+                        {
+                            fs.Close();
+                            br.Close();
+                            break;
+                        }
+                        else
                         {
                             //校验每一个字段 
                             //if (VerifyWorkDataMsg(buffer_msg))           //这里为了能读取字段，将校验屏蔽掉
@@ -4397,17 +4434,67 @@ namespace BreathingMachine
                                 {
                                     continue;
                                 }
-                                workDataMsg = GetObject<WORK_INFO_MESSAGE>(buffer_msg, len_msg);
-                                list.Add(workDataMsg);
-                                m_lastWorkMsg = workDataMsg;//保留最后一个工作信息，作为最新的信息，刷新到app基本信息中
+                                //debugCnt++;
+                                // if (debugCnt == 158)
+                                {
+                                    workDataMsg = GetObject<WORK_INFO_MESSAGE>(buffer_msg, len_msg);
+                                    //2018.11.06发现文件数据中年月日不对，先给下位机做过滤
+                                    if (workDataMsg.YEAR1 > 99 || workDataMsg.YEAR2 > 99 || workDataMsg.MONTH > 12 || workDataMsg.DAY > 31 ||
+                                        workDataMsg.HOUR > 23 || workDataMsg.MINUTE > 59 || workDataMsg.SECOND > 59)
+                                    {
+                                        continue;
+                                    }
+                                    list.Add(workDataMsg);
+                                    m_lastWorkMsg = workDataMsg;
+                                }
+
+                                //workDataMsg = GetObject<WORK_INFO_MESSAGE>(buffer_msg, len_msg);
+                                //list.Add(workDataMsg);
+                                //m_lastWorkMsg = workDataMsg;//保留最后一个工作信息，作为最新的信息，刷新到app基本信息中
                             }
                         }
-                        m_workHead_Msg_Map[alarmHead] = list;
-                        //m_workHead_Msg_Map.Add(alarmHead,list);
-                        fs.Close();
-                        br.Close();
                     }
-                    
+                    //如果经过过滤之后，List的count不为0，才添加到链表中
+                    if (list.Count != 0)
+                    {
+                        m_workHead_Msg_Map[alarmHead] = list;
+                    }
+                    //m_workHead_Msg_Map.Add(alarmHead,list);
+                    fs.Close();
+                    br.Close();
+
+                    #region
+                    //if (br.Read(buffer_msg, 0, len_msg) == 0)
+                    //{
+                    //    //do nothing
+                    //    fs.Close();
+                    //    br.Close();
+                    //}
+                    //else
+                    //{
+                    //    while (br.Read(buffer_msg, 0, len_msg) > 0)
+                    //    {
+                    //        //校验每一个字段 
+                    //        //if (VerifyWorkDataMsg(buffer_msg))           //这里为了能读取字段，将校验屏蔽掉
+                    //        {
+                    //            //CheckEachByte(buffer_msg);
+                    //            //下位机会出现0，0，0，0，下位机尚未修复这个问题，在这里先过滤掉
+                    //            if (IsByte0x00(buffer_msg))
+                    //            {
+                    //                continue;
+                    //            }
+                    //            workDataMsg = GetObject<WORK_INFO_MESSAGE>(buffer_msg, len_msg);
+                    //            list.Add(workDataMsg);
+                    //            m_lastWorkMsg = workDataMsg;//保留最后一个工作信息，作为最新的信息，刷新到app基本信息中
+                    //        }
+                    //    }
+                    //    m_workHead_Msg_Map[alarmHead] = list;
+                    //    //m_workHead_Msg_Map.Add(alarmHead,list);
+                    //    fs.Close();
+                    //    br.Close();
+                    //}
+                    #endregion
+
                 }
                 else
                 {
